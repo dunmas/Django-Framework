1bd1d16d000d0c6b485c19f871c153240b66e255
Danila <levafork@gmail.com>
2023-03-28 11:00:16 +0300

    Выполнил задание по запросам

--- 1.2-requests-templates/recipes/recipes/urls.py
+++ 1.2-requests-templates/recipes/recipes/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 
 from django.urls import path
 
+from calculator.views import dish_returner
+
 urlpatterns = [
-    # здесь зарегистрируйте вашу view-функцию
+    path('<str:dish>/', dish_returner)
 ]
