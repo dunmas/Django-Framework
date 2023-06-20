15ee85159ce9a6658dc527882431d1f2a5922dea
Danila <levafork@gmail.com>
2023-06-20 20:52:02 +0300

    Готов CRUD для модели Product

--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -15,20 +15,42 @@ class ProductViewSet(ModelViewSet):
 
         return Response({'detail': 'Product is added.'})
 
-    def retrieve(self, request, pk=None):
-        pass
+    def list(self, request):
+        arg = request.GET.get('search')
+        found_products = []
+        queryset = self.get_queryset()
 
-    # def list(self, request):
-    #     pass
+        if not arg:
+            serializer = self.get_serializer(queryset, many=True)
+            return Response(serializer.data)
 
-    def update(self, request, pk=None):
-        pass
+        for product in queryset:
+            if arg.casefold() in product.title.casefold() or arg.casefold() in product.description.casefold():
+                found_products.append(product)
 
-    # TODO: тут лажа, переделать
-    def destroy(self, request, pk=None):
+        if len(found_products) == 0:
+            return Response({'detail': f'There is no such products.'})
+
+        serializer = self.get_serializer(found_products, many=True)
+        return Response(serializer.data)
+
+    def partial_update(self, request, pk=None):
+        if not pk:
+            return Response({'detail': f'Request error.'})
+
         product = Product.objects.filter(id=pk)
+        if len(product) == 0:
+            return Response({'detail': f'There is no product №{pk}.'})
 
-        if len(product._result_cache) == 0:
+        product.update(description=request.data['description'])
+        return Response({'detail': f'Product №{pk} is updated.'})
+
+    def destroy(self, request, pk=None):
+        if not pk:
+            return Response({'detail': f'Request error.'})
+
+        product = Product.objects.filter(id=pk)
+        if len(product) == 0:
             return Response({'detail': f'There is no product №{pk}.'})
 
         product.delete()
