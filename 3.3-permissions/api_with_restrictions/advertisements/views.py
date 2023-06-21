de1f0e2bdcccb0dc9c9f480dc90269f11a6cfeea
Danila <levafork@gmail.com>
2023-06-21 14:54:35 +0300

    Общие действия по получению работоспособности приложения, работают получение и создание объявлений, изменение только тем пользователем, который создал это объявление

--- 3.3-permissions/api_with_restrictions/advertisements/views.py
+++ 3.3-permissions/api_with_restrictions/advertisements/views.py
@@ -1,6 +1,11 @@
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
+from advertisements.filters import AdvertisementFilter
+from advertisements.permissions import IsOwnerOrReadOnly
+from advertisements.models import Advertisement
+from advertisements.serializers import AdvertisementSerializer
+
 
 class AdvertisementViewSet(ModelViewSet):
     """ViewSet для объявлений."""
@@ -8,8 +13,16 @@ class AdvertisementViewSet(ModelViewSet):
     # TODO: настройте ViewSet, укажите атрибуты для кверисета,
     #   сериализаторов и фильтров
 
-    def get_permissions(self):
-        """Получение прав для действий."""
-        if self.action in ["create", "update", "partial_update"]:
-            return [IsAuthenticated()]
-        return []
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    # filter_backends = [AdvertisementFilter]
+
+    # def get_permissions(self):
+    #     """Получение прав для действий."""
+    #     if self.action in ["create", "update", "partial_update"]:
+    #         return [IsAuthenticated()]
+    #     return []
+
+    def perform_create(self, serializer):
+        serializer.save(creator=self.request.user)
